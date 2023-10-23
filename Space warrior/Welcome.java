3bd0e3ccfe5a412700d53ea88d37d40baf9f5730
Joy Yahshua <85402808+laggy-tux@users.noreply.github.com>
2023-10-23 17:00:49 +0530

    added sums and other minor tweaks

---
+++ Space warrior/Welcome.java
@@ -0,0 +1,15 @@
+public class Welcome {
+  void welcome()
+  {
+   
+   System.out.println("                                                          p - Play");
+   System.out.println();
+   System.out.println("                                                          h - high-score ");
+   System.out.println();
+   System.out.println("                                                          q - quit ");
+   System.out.println();
+   System.out.println("                                                          r - rules ");
+   
+   
+  }
+}
